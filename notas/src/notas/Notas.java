d573cd72874ccf9d3ca97645627702fbd7dc326a
tzeen <tzeen@192.168.56.1>
2023-02-01 18:15:53 +0100

    Notas arreglado tras PMD(apartado 2)

--- notas/src/notas/Notas.java
+++ notas/src/notas/Notas.java
@@ -2,27 +2,29 @@ package notas;
 
 import java.util.Scanner;
 
-public class Notas {
+//clase para comprobar las notas de un alumno
+public class Notas { // NOPMD by tzeen on 1/2/23 18:11
 
-public static void main(String[] args) {
-Scanner reader = new Scanner(System.in);// Para pedir un dato numérico por teclado
-int n = 0;
-String c = "";
-System.out.print("Introduzca el valor de una calificación: ");
-n = reader.nextInt();
+//clase main
+public static void main(final String[] args) {//comienza la ejecucion // NOPMD by tzeen on 1/2/23 18:14
 
-if (n >= 0 && n < 5)
-c = "Suspenso";
-else if (n >= 5 && n < 7)
-c = "Aprobado";
-else if (n >= 7 && n <9)
-c = "Notable";
-else if (n >= 9 && n <= 10)
-c = "Sobresaliente";
-else
-c = "El valor de la calificación introducida no es correcta";
-
-System.out.println(c);
+// Para pedir un dato numérico por teclado	
+final Scanner reader = new Scanner(System.in); // NOPMD by tzeen on 1/2/23 18:14
+String calificacion;
+System.out.println("Introduzca el valor de una calificación: "); // NOPMD by tzeen on 1/2/23 18:14
+int nota = reader.nextInt(); // NOPMD by tzeen on 1/2/23 18:14
 reader.close();
+if (nota >= 0 && nota< 5) {	calificacion = "Suspenso";}
+
+else if (nota >= 5 && nota< 7){	calificacion = "Aprobado";}
+
+else if (nota >= 7 && nota <9){	calificacion = "Notable";}
+
+else if (nota >= 9 && nota<= 10){	calificacion = "Sobresaliente";}
+
+else {calificacion = "El valor de la calificación introducida no es correcta";}
+
+System.out.println(calificacion); // NOPMD by tzeen on 1/2/23 18:15
+
 }
 }
