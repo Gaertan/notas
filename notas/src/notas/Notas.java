e2178a4d0b6013d579ac660bf52e72acefc1879f
tzeen <tzeen@192.168.56.1>
2023-02-01 18:32:21 +0100

    Utiliza las funciones de refactorización para cambiar la cabecera del método "cali". Le cambiarás el nombre por "califica" y, en el mismo paso, cambiarás el nombre del parámetro por "num_nota". El método anterior tendrá que quedar en desuso. (En Eclipse). Sube las modificaciones al repositorio de Github, siendo el comentario el enunciado de este ejercicio.

--- notas/src/notas/Notas.java
+++ notas/src/notas/Notas.java
@@ -13,22 +13,30 @@ final Scanner reader = new Scanner(System.in);
 System.out.println("Introduzca el valor de una calificación: "); 
 int notaAlumno = reader.nextInt(); 
 reader.close();
-cali(notaAlumno); 
+califica(notaAlumno); 
 
 }
 
 /**aqui el comentario, el código dicta la calificacion dependiendo del parametro de la nota del alumno
  * @param notaAlumno
+ * @deprecated Use {@link #califica(int)} instead
  */
 public static void cali(int notaAlumno) {
+	califica(notaAlumno);
+}
+
+/**aqui el comentario, el código dicta la calificacion dependiendo del parametro de la nota del alumno
+ * @param num_nota
+ */
+public static void califica(int num_nota) {
 	String calificacionAlumno;
-	if (notaAlumno >= 0 && notaAlumno< 5) {	calificacionAlumno  = "Suspenso";}
+	if (num_nota >= 0 && num_nota< 5) {	calificacionAlumno  = "Suspenso";}
 	
-	else if (notaAlumno >= 5 && notaAlumno< 7){	calificacionAlumno  = "Aprobado";}
+	else if (num_nota >= 5 && num_nota< 7){	calificacionAlumno  = "Aprobado";}
 	
-	else if (notaAlumno >= 7 && notaAlumno <9){	calificacionAlumno  = "Notable";}
+	else if (num_nota >= 7 && num_nota <9){	calificacionAlumno  = "Notable";}
 	
-	else if (notaAlumno >= 9 && notaAlumno<= 10){	calificacionAlumno  = "Sobresaliente";}
+	else if (num_nota >= 9 && num_nota<= 10){	calificacionAlumno  = "Sobresaliente";}
 	
 	else {calificacionAlumno  = "El valor de la calificación introducida no es correcta";}
 	
