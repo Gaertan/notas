4c392dbf0c9c58e87eefbda8ba41cb8a8d2c61db
tzeen <tzeen@192.168.56.1>
2023-02-01 18:28:22 +0100

    Utiliza las funciones de refactorización para sustituir el código que se encarga de especificar la calificación en función de la nota por un método que llamarás "cali", que será público, de forma que se incluyan los comentarios para la documentación si ello es posible. Muestra cómo se hace en los dos entornos. (En Eclipse). Sube las modificaciones al repositorio de Github, siendo el comentario el enunciado de este ejercicio.

--- notas/src/notas/Notas.java
+++ notas/src/notas/Notas.java
@@ -3,28 +3,35 @@ package notas;
 import java.util.Scanner;
 
 //clase para comprobar las notas de un alumno
-public class Notas { // NOPMD by tzeen on 1/2/23 18:11
+public class Notas {
 
 //clase main
-public static void main(final String[] args) {//comienza la ejecucion // NOPMD by tzeen on 1/2/23 18:14
+public static void main(final String[] args) {//comienza la ejecucion 
 
 // Para pedir un dato numérico por teclado	
-final Scanner reader = new Scanner(System.in); // NOPMD by tzeen on 1/2/23 18:14
-String calificacionAlumno ;
-System.out.println("Introduzca el valor de una calificación: "); // NOPMD by tzeen on 1/2/23 18:14
-int notaAlumno = reader.nextInt(); // NOPMD by tzeen on 1/2/23 18:14
+final Scanner reader = new Scanner(System.in); 
+System.out.println("Introduzca el valor de una calificación: "); 
+int notaAlumno = reader.nextInt(); 
 reader.close();
-if (notaAlumno >= 0 && notaAlumno< 5) {	calificacionAlumno  = "Suspenso";}
+cali(notaAlumno); 
 
-else if (notaAlumno >= 5 && notaAlumno< 7){	calificacionAlumno  = "Aprobado";}
+}
 
-else if (notaAlumno >= 7 && notaAlumno <9){	calificacionAlumno  = "Notable";}
-
-else if (notaAlumno >= 9 && notaAlumno<= 10){	calificacionAlumno  = "Sobresaliente";}
-
-else {calificacionAlumno  = "El valor de la calificación introducida no es correcta";}
-
-System.out.println(calificacionAlumno ); // NOPMD by tzeen on 1/2/23 18:15
-
+/**aqui el comentario, el código dicta la calificacion dependiendo del parametro de la nota del alumno
+ * @param notaAlumno
+ */
+public static void cali(int notaAlumno) {
+	String calificacionAlumno;
+	if (notaAlumno >= 0 && notaAlumno< 5) {	calificacionAlumno  = "Suspenso";}
+	
+	else if (notaAlumno >= 5 && notaAlumno< 7){	calificacionAlumno  = "Aprobado";}
+	
+	else if (notaAlumno >= 7 && notaAlumno <9){	calificacionAlumno  = "Notable";}
+	
+	else if (notaAlumno >= 9 && notaAlumno<= 10){	calificacionAlumno  = "Sobresaliente";}
+	
+	else {calificacionAlumno  = "El valor de la calificación introducida no es correcta";}
+	
+	System.out.println(calificacionAlumno );
 }
 }
